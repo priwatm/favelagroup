067e36cde81ca312f5c74ac0981741e580fc0801
Сердар Коробейников <priwatm@gmail.com>
2021-09-01 18:43:47 +0300

    first commit

--- src/components/pages/ForgetPassword/components/EmailSent/index.tsx
+++ src/components/pages/ForgetPassword/components/EmailSent/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import {observer} from "mobx-react";
-import {useHistory} from "react-router-dom";
 import {routes} from "src/constants/routes";
 
-export const EmailSent = observer(()=>{
-    const history = useHistory();
-    return(
+export interface EmailSentProps {
+    onSubmit: () => void
+}
+
+export const EmailSent:React.ComponentType<EmailSentProps> = observer(({onSubmit}) => {
+    return (
         <>
             <h1>Письмо отправлено</h1>
             <h2>На указанный вами e-mail было отправлено письмо для смены пароля</h2>
             <div className="forget-container__submit-group">
-                <button onClick={()=>{history.push(routes.auth.path)}} className="btn btn-blue">Вернуться на главную</button>
+                <button onClick={onSubmit} className="btn btn-blue">Вернуться на главную
+                </button>
             </div>
         </>
     )
--- src/components/pages/ForgetPassword/index.tsx
+++ src/components/pages/ForgetPassword/index.tsx
@@ -5,29 +5,29 @@ import {ForgetState} from "src/components/pages/ForgetPassword/components/types"
 import {ForgetPass} from 'src/components/pages/ForgetPassword/components/ForgetPassword'
 import {EmailSent} from 'src/components/pages/ForgetPassword/components/EmailSent'
 import {useApplicationManager} from "src/hooks/manager/useApplicationManager";
+import {routes} from "src/constants/routes";
+import {useHistory} from "react-router-dom";
+
 
 export const ForgetSend = observer(() => {
-    const applicationManager = useApplicationManager()
     const [component, setComponent] = useState<ForgetState>(ForgetState.ForgetPassword);
     const [icon, setIcon] = useState('icon__key')
-    const {visibleHeader} = applicationManager;
-
-    function effectInitial() {
-        visibleHeader.visible = false;
-    }
-
-    useEffect(effectInitial, []);
+    const history = useHistory();
 
     const onEmailSent = () => {
         setComponent(ForgetState.EmailSent);
         setIcon('icon__send');
     }
 
+    const onReturn = () => {
+        history.push(routes.auth.path)
+    }
+
     return (
         <div className="forget-container">
             <i className={icon} />
             {component === ForgetState.ForgetPassword && <ForgetPass onControl={onEmailSent} />}
-            {component === ForgetState.EmailSent && <EmailSent />}
+            {component === ForgetState.EmailSent && <EmailSent onSubmit={onReturn}/>}
 
         </div>
     )
